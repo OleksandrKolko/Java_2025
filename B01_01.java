89cc0bb367f8c26f12acd66de0c5bc78744c136b
OleksandrKolko <144420883+OleksandrKolko@users.noreply.github.com>
2025-09-15 22:41:03 +0300

    Delete B01_01.java

--- B01_01.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class B01_01 {
-    public static void main(String[] args) {
-        System.out.println("Введіть ім’я: ");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Привіт, " + scanner.next());
-    }
-}
